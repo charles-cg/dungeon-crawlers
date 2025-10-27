8700f7194824ab90c694b49d9460c59647b2a3c7
Carlos C <carlitoscarreong@hotmail.com>
2025-10-27 13:08:38 -0600

    added setters and getters for the Square class

--- Square.h
+++ Square.h
@@ -9,6 +9,25 @@ private:
     std::string name;
     unsigned int encounterChance; // chance ranges from 0 - 1
     bool wasVisited; // flag indicating if a square has been visited
+    Square<T>* prev;
+    Square<T>* next;
+public:
+    Square();
+    ~Square();
+
+    void setId(const int& id);
+    void setName (const std::string& name);
+    void setEncounterChance(const int& encounterChance);
+    void setWasVisited(bool& wasVisited);
+    void setPrev(Square<T>* prev);
+    void setNext(Square<T>* next);
+
+    unsigned int getId();
+    std::string getName();
+    unsigned int getEncounterChance();
+    bool getWasVisited();
+    Square<T>* getPrev();
+    Square<T>* getNext();
 };
 
 #endif
