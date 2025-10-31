b7ac0bef2f736a2e3f20c761c54278abe8a43e1d
Carlos C <carlitoscarreong@hotmail.com>
2025-10-31 13:20:56 -0600

    added codex header file with tree attribute and loadMonster method declaration, created codex cpp file

---
+++ Codex.cpp
@@ -0,0 +1,9 @@
+#pragma once
+#include <iostream>
+#include "Codex.h"
+#include <fstream>
+#include <sstream>
+
+bool loadMonsterTXT(std::string filename) {
+    
+}
--- Codex.h
+++ Codex.h
@@ -6,8 +6,14 @@ template <typename T>
 class Codex {
 private:
     BST<T> tree;
+
+    bool loadMonsterTXT(std::string filename);
+
 public:
-    
+    Codex();
+    ~Codex();
+
+    bool loadMonsterData();
 };
 
 #endif
