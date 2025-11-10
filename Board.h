939fc3615ea9a4210e64847606f5f19ffce822a7
mateohernandez <a01660938@tec.mx>
2025-11-09 20:40:31 -0600

    Added Constructor and Destructor in board.h

--- Board.h
+++ Board.h
@@ -8,7 +8,33 @@
 
 template <typename T>
 class Board {
+private:
+    int n;
+    int count;
+    T* vertex;
+    int** Board; // This is the matrix
+    int indexOf(cont T& v) const;
+public:
+    Graph(int capacity): n(capacity), count(0) {
+          vetrex = new T[n];
+          Board = new int*[n];
+          for (int i = 0; i < n; i++) {
+               Board[i] = new int[n];
+               for (int j = 0; j < n; j++) {
+                    Board[i][j] = 0;
+               }
+          }
+    }
 
+~Graph() {
+for (int i = 0; i < n; i++) {
+delete[] Board[i];
+}
+delete[] Board;
+delete[] vetrex;
+}
+bool addVertex(const T& v);
+bool addEdge(const T& u, const T& v, int weight = 1, bool directed = false);
 };
 
 
