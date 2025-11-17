52d8515b511cca028a1264c5e5206870a03ce930
Carlos C <carlitoscarreong@hotmail.com>
2025-11-17 01:25:34 -0600

    bug fixing move hero error checking

--- Dungeon.cpp
+++ Dungeon.cpp
@@ -230,7 +230,8 @@ bool Dungeon::moveHero(const std::string& roomId) {
 	Room<Monster> tempRoom(roomId);
 	Room<Monster> currentRoom(hero->getCurrentRoomId());
 	Edge<Room<Monster>> searchEdge(tempRoom, 0);
-	if (!board.findVertexNode(tempRoom)->getData().getAdj().search(searchEdge)) {
+	// Check if targetRoom is adjacent to currentRoom (not the other way around)
+	if (!board.findVertexNode(currentRoom)->getData().getAdj().search(searchEdge)) {
 		std::cout << "Where did you even read that Magic ID, try again!" << std::endl;
 		return false;
 	}
